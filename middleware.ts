e8a931cc7bf0ca2f3de1d0ddfe8725d6c2e3f9bf
dineshvarma763 <dinesh@conversiondigital.com.au>
2024-08-12 19:28:45 +0530

    V3Aug12

--- middleware.ts
+++ middleware.ts
@@ -6,9 +6,12 @@ export async function middleware(req: NextRequest) {
     
     const path = req.nextUrl.pathname
     const temp = req.headers['referer']
-    const nextUrl = req.headers['next-url']; // Case-insensitive, always lowercase  
-    console.log("Next-Url:", nextUrl , path);
-    console.log("checking referer url", temp , path)
+    const headers = req.headers;
+
+    // Log all headers
+    headers.forEach((value, key) => {
+      console.log(`${key}: ${value}`);
+    });
     const newdomain = process.env.ATA_WEBSITE_DOMAIN || 'ata-git-production-conversion-digital.vercel.app'
    const baseUrl = process.env.NEXT_PUBLIC_BASE_URL || 'http://localhost:3000'; 
     //const baseUrl = 'norg-cookie-july-lfk9-5ro7ldl6v-dineshvarmas-projects.vercel.app';
